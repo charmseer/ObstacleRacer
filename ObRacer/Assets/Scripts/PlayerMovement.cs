7c88ed65f92c487c80e71018dc2dd60fa0be9df6
charmseer <charmseer@gmail.com>
2021-05-17 23:35:31 +0530

    Add Movement to Player

---
+++ ObRacer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 200 * Time.deltaTime);
+    }
+}
