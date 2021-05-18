cd09d0fcc76576a128306200b991242dceddde00
charmseer <charmseer@gmail.com>
2021-05-18 19:28:43 +0530

    Add sideways motion

--- ObRacer/Assets/Scripts/PlayerMovement.cs
+++ ObRacer/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
-    // Start is called before the first frame update
+    public float forwardForce = 1000f;
+    public float sidewaysForce = 750f;
+    // Start is called before the first Frame;
     void Start()
     {
         
@@ -13,6 +15,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 200 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce*Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
