96298014d33c567828cc8861b60fc6f6a9ab08a1
charmseer <charmseer@gmail.com>
2021-05-20 12:40:25 +0530

    Add feature - Die when fall off ground

--- ObRacer/Assets/Scripts/PlayerMovement.cs
+++ ObRacer/Assets/Scripts/PlayerMovement.cs
@@ -25,5 +25,10 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
+
+        if (rb.position.y < -1.5f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
     }
 }
