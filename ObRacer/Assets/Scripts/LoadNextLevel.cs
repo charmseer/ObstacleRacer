4bb9112e3f9a973e83a2b13b66c50b5c6e649ca5
charmseer <charmseer@gmail.com>
2021-05-20 20:39:57 +0530

    Add Animation Event after Level Complete to Load next Scene
    We attach a Script to Level Complete Panel, as it is one being animated. The Level complete panel becomes Active when Player touches EndGame Object. First the Animation plays. keep the Animation Event  after it. In the Inspector you can choose the function to run. Since the script is attached to LevelCompletePanel, you can add the Load Next function. You need to create prefabs of player and other essential elements so that they can be modified uniformly across levels when needed. Then Duplicate lvl1 Add the new scenes in the Build settings.

---
+++ ObRacer/Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    public void LoadNext()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+}
