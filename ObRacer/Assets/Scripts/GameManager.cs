b685bc1f1583de2a05da5770f9d8bd4a9aaba6d2
charmseer <charmseer@gmail.com>
2021-06-08 23:44:06 +0530

    Implement Pause Menu and Esc Menu

--- ObRacer/Assets/Scripts/GameManager.cs
+++ ObRacer/Assets/Scripts/GameManager.cs
@@ -8,6 +8,26 @@ public class GameManager : MonoBehaviour
 
     public float restartDelay = 1f;
     public GameObject completeLvlUI;
+
+    //Pause Button Panel
+    public GameObject PauseMenuUI;
+
+    //Esc Key Back button panel
+    public GameObject EscMenuPanel;
+
+    private void Update()
+    {
+
+        //If Player presses Esc button or back button on Android phones
+        if (UnityEngine.InputSystem.Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            Debug.Log("Escape Is Pressed");
+            EscMenuPanel.SetActive(true);
+            Time.timeScale = 0f;
+            //ExitMenu();
+        }
+
+    }
     public void EndGame()
     {
         if(gameHasEnded == false)
@@ -16,10 +36,39 @@ public class GameManager : MonoBehaviour
             Debug.Log("Game Over");
             Invoke("Restart", restartDelay) ;
         }
-        
+
     }
-    
-    
+
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        //GameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        //GameIsPaused = false;
+    }
+
+    public void EscResume()
+    {
+        EscMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void MainMenu()
+    {
+        PauseMenuUI.SetActive(false);
+        // When you hit Pause Time Scale freezes, So before you go to Main Menu you have to unfreeze it
+        Time.timeScale = 1f;
+        //GameIsPaused = false;
+        SceneManager.LoadScene("Menu");
+    }
+
+
     public void CompleteLevel()
     {
         Debug.Log("Level Won!");
@@ -29,4 +78,12 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    public void QuitGame()
+    {
+        Debug.Log("Player has chosen to quit the game");
+        Application.Quit();
+    }
 }
+
+
