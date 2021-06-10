fb2222d8a898a7b5a65a22de831e473f7904dfc6
charmseer <charmseer@gmail.com>
2021-06-10 17:16:41 +0530

    Stop Player Movement OnTriggerEnter with the EndLevel Block

--- ObRacer/Assets/Scripts/EndTrigger.cs
+++ ObRacer/Assets/Scripts/EndTrigger.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class EndTrigger : MonoBehaviour
 {
+    public GameObject Player;
     public GameManager gameManager;
     private void OnTriggerEnter()
     {
+        Player.GetComponent<PlayerController>().enabled = false;
         gameManager.CompleteLevel();   
     }
  
