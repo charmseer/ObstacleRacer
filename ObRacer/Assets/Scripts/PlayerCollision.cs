dffbdb1c0901d1f74dbe62faccaa9628e3c839b9
charmseer <charmseer@gmail.com>
2021-06-16 13:26:43 +0530

    Set up Individual Highscore system for each level

--- ObRacer/Assets/Scripts/PlayerCollision.cs
+++ ObRacer/Assets/Scripts/PlayerCollision.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour
 {
-    public PlayerController movement;
-    public GameObject CollisionPanel;
-    public int theScore;
-    public Text scoreValue;
-    public Text scoreValueLvlEnd;
-    public Text bestScoreValue;
+    public PlayerController movement; // Gets the player's Movement Script
+    public GameObject CollisionPanel; // To bring up the Panel on Collision
+    public int theScore; // To score the score
+    public Text scoreValue; // On Screen Score Text, Updates live
+    public Text scoreValueLvlEnd; // Score shown at the end of the Level
+    public Text bestScoreValue; // High Score shown at the end of the level
+    //public Text bestScoreValue2;
 
     private void Start()
     {
-        bestScoreValue.text = PlayerPrefs.GetInt("BestScore", 000).ToString();
+        //In the beginning check the Scene name and set the bestScoreValue based on 
+        //the PlayerPref variable of the corresponding Scene
+        if(SceneManager.GetActiveScene().name == "World1Level1")
+        {
+            bestScoreValue.text = PlayerPrefs.GetInt("World1Level1", 000).ToString();
+        }
+        if (SceneManager.GetActiveScene().name == "World1Level2")
+        {
+            bestScoreValue.text = PlayerPrefs.GetInt("World1Level2", 000).ToString();
+        }
+
+        if (SceneManager.GetActiveScene().name == "World1Level3")
+        {
+            bestScoreValue.text = PlayerPrefs.GetInt("World1Level3", 000).ToString();
+        }
+        //bestScoreValue.text = PlayerPrefs.GetInt("World1Level2", 000).ToString();
+
     }
     private void OnCollisionEnter(Collision collisionInfo)
     {
@@ -35,20 +53,40 @@ public class PlayerCollision : MonoBehaviour
     {
         if (other.gameObject.tag == "Token")
         {
-            //Had to add the if statement here because the EndLevel Invisible Block isTrigger.
+            //Had to add the if statement here because the EndLevel Invisible Block has 'isTrigger' enabled.
             // So when the player collides with the endlevel Block the score goes up by 1.
-            // Cant have that now can we.
+            // Cant have that now can we?
             theScore += 1;
 
             scoreValue.GetComponent<Text>().text = theScore.ToString() + " / ";
             scoreValueLvlEnd.GetComponent<Text>().text = theScore.ToString()+ " / ";
 
         }
-        if (theScore > PlayerPrefs.GetInt("BestScore", 0))
+        if (SceneManager.GetActiveScene().name == "World1Level1")
         {
-            PlayerPrefs.SetInt("BestScore", theScore);
-            bestScoreValue.text = theScore.ToString() + " / ";
+            if (theScore > PlayerPrefs.GetInt("World1Level1", 0))
+            {
+                PlayerPrefs.SetInt("World1Level1", theScore);
+                bestScoreValue.text = theScore.ToString() + " / ";
+            }
+        }
+        
+        if (SceneManager.GetActiveScene().name == "World1Level2")
+        {
+            if (theScore > PlayerPrefs.GetInt("World1Level2", 0))
+            {
+                PlayerPrefs.SetInt("World1Level2", theScore);
+                bestScoreValue.text = theScore.ToString() + " / ";
+            }
+        }
 
+        if (SceneManager.GetActiveScene().name == "World1Level3")
+        {
+            if (theScore > PlayerPrefs.GetInt("World1Level3", 0))
+            {
+                PlayerPrefs.SetInt("World1Level3", theScore);
+                bestScoreValue.text = theScore.ToString() + " / ";
+            }
         }
 
     }
