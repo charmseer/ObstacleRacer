34bc48054ff761d66f1d6bb7638c175d8070a077
charmseer <charmseer@gmail.com>
2021-07-12 12:06:26 +0530

    Make End Credits script to add On Click events to buttons

---
+++ ObRacer/Assets/Scripts/EndCredits.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCredits : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("Player has chosen to quit the game");
+        Application.Quit();
+    }
+    public void MainMenu()
+    {
+        //GameIsPaused = false;
+        SceneManager.LoadScene("Menu");
+    }
+}
